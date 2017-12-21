3da7cd402de4c8de0959c200c06c9da243ab0c18
Matt <weidman.matthew@gmail.com>
2017-12-21 14:19:47 -0700

    list of primes

--- sha256.cpp
+++ sha256.cpp
@@ -3,9 +3,10 @@
 #include <fstream>
 #include <iostream>
 #include <math.h>
-#include <string>
+#include <sstream>
 #include <stdlib.h>
 #include <string.h>
+#include <string>
 using namespace std;
 
 /** Returns the contents of the file at path [fileName]. */
@@ -43,9 +44,70 @@ int fractOfRoot(double x, double y) {
   return fplong >> 20;
 }
 
+/**
+ * Computes the list of prime numbers up to and including p
+ * using the Sieve of Eratosthenes.
+ */
+int* primesTo(int p) {
+  // create array of numbers 1 to p
+  bool* isPrime = new bool[p];
+  memset(isPrime, 1, p);
+
+  double sqrtp = sqrt(p);
+
+  // label each composite number
+  isPrime[0] = false;
+  for (int i=1; i<sqrtp; i++) {
+    int factor = i+1;
+    if (isPrime[i]) {
+      for (int j = i + factor; j < p; j += factor) {
+        isPrime[j] = false;
+      }
+    }
+  }
+
+  // get number of primes
+  int numPrimes = 0;
+  for (int i=0; i<p; i++) {
+    if (isPrime[i]) numPrimes++;
+  }
+
+  // create array of primes
+  int* primes = new int[numPrimes];
+  int j = 0;
+  for (int i=0; i<p; i++) {
+    if (isPrime[i]) {
+      primes[j] = i+1;
+      j++;
+    }
+  }
+  return primes;
+}
+
+/* Converts int to string. */
+string intToString(int x) {
+  ostringstream s;
+  s << x;
+  return s.str();
+}
+
+/**
+ * Takes an array [i_1, i_2, ..., i_n] and length.
+ * Returns the string "[i_1, i_2, ..., i_length]".
+ */
+string intArrToString(int* arr, int length) {
+  string ans = "[";
+  for (int i=0; i<length; i++) {
+    ans = ans + intToString(arr[i]);
+    if (i != length-1) ans += ", ";
+  }
+  return ans + "]";
+}
+
 int main()
 {
   // const char* fileName = "data.txt";
   // char* buffer = getFileContents(fileName);
-  cout << hex << fractOfRoot(311, 3);
+  int* primes = primesTo(311);
+  cout << intArrToString(primes, 64) << endl;
 }
